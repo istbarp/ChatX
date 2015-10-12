4bcd03551d7b8e752addcdcf266b8f4ac101799d
Michael Dolatko <sayba_killer@hotmail.com>
2015-10-12 13:34:32 +0200

    ASDF

--- ChatX/Server/Model/Message.cs
+++ ChatX/Server/Model/Message.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class Message
+    public class Message
     {
+        private string text;
+        private User sender;
+        private int roomID;
+
+        public Message()
+        {
+
+        }
+
+        public Message(string text, User sender, int roomID)
+        {
+            this.text = text;
+            this.sender = sender;
+            this.roomID = roomID;
+        }
+
+        public string Text 
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        public User Sender
+        {
+            get { return sender; }
+            set { sender = value; }
+        }
+
+        public int RoomID
+        {
+            get { return roomID; }
+            set { roomID = value; }
+        }
     }
 }
--- ChatX/Server/Model/User.cs
+++ ChatX/Server/Model/User.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class User
+    public class User
     {
+        private string userName;
+        private string password;
+
+        public User()
+        {
+
+        }
+
+        public User(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
     }
 }
